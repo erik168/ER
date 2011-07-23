4e5912740ee67888a21e961593253568d432d369
errorrik <erik168@163.com>
2011-07-23 18:41:34 +0800

    revise Link 4 standard

--- src/esui/Link.js
+++ src/esui/Link.js
@@ -2,68 +2,112 @@
  * ESUI (Enterprise Simple UI)
  * Copyright 2010 Baidu Inc. All rights reserved.
  * 
- * path:    ui/Link.js
+ * path:    esui/Link.js
  * desc:    链接控件
- * author:  zhaolei,erik
- * date:    $Date$
+ * author:  zhaolei, erik
  */
+
+
+///import esui.Control;
+///import baidu.string.encodeHTML;
+///import baidu.lang.inherits
 
 /**
  * 链接控件
  * 
+ * @class
  * @param {Object} options 控件初始化参数
  */
-ui.Link = function (options) {
-    // 初始化参数
-    this.__initOptions(options);
-    
+esui.Link = function ( options ) {
     // 类型声明，用于生成控件子dom的id和class
     this._type = 'link';
+
+    // 标识鼠标事件触发自动状态转换
+    this._autoState = 0;
+
+    esui.Control.call( this, options );
 };
 
-ui.Link.prototype = {
+esui.Link.prototype = {
     /**
      * 渲染控件
      * 
      * @public
      * @param {Object} main 控件挂载的DOM
      */
-    render: function (main) {
+    render: function () {
         var me = this;
-        ui.Base.render.call(me, main, true);
+        esui.Control.prototype.render.call( me );
         
-        if ( me._main ) {
-            // NOTE 如果没有me.herf，就不需要给链接添加href属性了，否则
-            // 就成了<a href="undefined"></a>了
-            me.href && (me._main.href = me.href);
-            me.text && (me._main.innerHTML = me.text);
-            me.target && (me._main.target = me.target);
-        }
+        // 设置各种属性
+        me.href    && me.setHref( me.href );
+        me.text    && me.setText( me.text );
+        me.content && me.setContent( me.content );
+        me.target  && me.setTarget( me.target );
         
+        // 绑定点击事件处理
         if ( !me._clickHandler ) {
             me._clickHandler = me._getClickHandler();
-            me._main.onclick = me._clickHandler;
+            me.main.onclick = me._clickHandler;
         }
+    },
+
+    /**
+     * 设置链接地址
+     *
+     * @public
+     * @param {string} href 链接地址
+     */
+    setHref: function ( href ) {
+        !href && ( href = '' );
+        me.main.href = href;
+    },
+
+    /**
+     * 设置链接显示文字。经过html encode
+     *
+     * @public
+     * @param {string} text 显示文字
+     */
+    setText: function ( text ) {
+        !text && ( text = '' );
+        this.setContent( baidu.encodeHTML( text ) );
     },
     
     /**
-     * 将控件添加到某个dom元素中
-     * 
+     * 设置链接显示内容。不经过html encode
+     *
      * @public
-     * @param {HTMLElement} opt_wrap 目标dom
+     * @param {string} content 链接显示内容
      */
-    appendTo: function (wrap) {
-        if (this._main) {
-            return;
-        }
+    setContent: function ( content ) {
+        !content && ( content = '' );
+        this.main.innerHTML = content;
+    },
 
-        var main = document.createElement('a');
-        wrap.appendChild(main);
-        this.render(main);
+    /**
+     * 设置链接target
+     *
+     * @public
+     * @param {string} target 链接target
+     */
+    setTarget: function ( 链接target ) {
+        !target && ( target = '' );
+        this.main.target = target;
     },
     
     onclick: new Function(),
     
+    /**
+     * 生成控件主元素
+     *
+     * @protected
+     * @return {HTMLElement}
+     */
+    __createMain: function () {
+        return document.createElement( 'a' );
+    },
+
     /**
      * 获取点击的handler
      * 
@@ -72,21 +116,21 @@ ui.Link.prototype = {
      */
     _getClickHandler: function() {
         var me = this;
-        return function (e) {
-            return me.onclick(e);
+        return function ( e ) {
+            return me.onclick( e );
         };
     },
     
     /**
-     * 释放控件
+     * 销毁控件
      * 
-     * @public
+     * @private
      */
-    dispose: function () {
+    __dispose: function () {
         this._clickHandler = null;
-        this._main.onclick = null;
-        ui.Base.dispose.call(this);
+        esui.Control.prototype.__dispose.call( this );
     }
 };
 
-ui.Base.derive(ui.Link);
+baidu.inherits( esui.Link, esui.Control );
+
