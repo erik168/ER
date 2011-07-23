700f83c58418ec2fb6da8ef9b9237bc8431da889
errorrik <erik168@163.com>
2011-07-23 13:20:15 +0800

    add esui namespace declartion

---
+++ src/esui.js
@@ -0,0 +1,10 @@
+/*
+ * ESUI (Enterprise Simple UI)
+ * Copyright 2010 Baidu Inc. All rights reserved.
+ * 
+ * path:    esui.js
+ * desc:    esui是一套简单的WEB UI库
+ * author:  erik
+ */
+
+var esui = {};
