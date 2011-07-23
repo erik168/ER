c85b9ff8d64cdd47935085687b18f11b9d774927
errorrik <erik168@163.com>
2011-07-23 14:54:16 +0800

    add create shortcut method

---
+++ src/esui/create.js
@@ -0,0 +1,20 @@
+/*
+ * ESUI (Enterprise Simple UI)
+ * Copyright 2010 Baidu Inc. All rights reserved.
+ * 
+ * path:    esui/create.js
+ * desc:    创建控件
+ * author:  erik
+ */
+
+///import esui.util;
+
+/**
+ * 创建控件
+ * 
+ * @public
+ * @param {string} type 控件类型
+ * @param {Object} options 控件初始化参数
+ * @return {esui.Control} 创建的控件对象
+ */
+esui.create = esui.util.create;
