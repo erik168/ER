dd2dbf43caa9d6408ceddccbd9600aed700a2aba
errorrik <erik168@163.com>
2011-07-23 14:52:19 +0800

    add init shortcut method

---
+++ src/esui/init.js
@@ -0,0 +1,21 @@
+/*
+ * ESUI (Enterprise Simple UI)
+ * Copyright 2010 Baidu Inc. All rights reserved.
+ * 
+ * path:    esui/init.js
+ * desc:    初始化控件
+ * author:  erik
+ */
+
+///import esui.util;
+
+/**
+ * 初始化控件
+ * 
+ * @public
+ * @param {HTMLElement} opt_wrap 渲染的区域容器元素
+ * @param {Object}      opt_propMap 控件附加属性值
+ * @param {Function}    opt_attrReplacer 属性替换函数
+ * @return {Object} 控件集合
+ */
+esui.init = esui.util.init;
