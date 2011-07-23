62b06c6aa748eef1d28670fed5cfb9415cacad60
errorrik <erik168@163.com>
2011-07-23 14:55:44 +0800

    add dispose shortcut method

---
+++ src/esui/dispose.js
@@ -0,0 +1,18 @@
+/*
+ * ESUI (Enterprise Simple UI)
+ * Copyright 2010 Baidu Inc. All rights reserved.
+ * 
+ * path:    esui/dispose.js
+ * desc:    销毁控件
+ * author:  erik
+ */
+
+///import esui.util;
+
+/**
+ * 销毁控件
+ * 
+ * @public
+ * @param {Object|string} ctrl 控件或控件id
+ */
+esui.dispose = esui.util.dispose;
