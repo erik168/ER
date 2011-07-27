3a69a9deb036e519e773f44b8ad83fe681ee9ade
errorrik <erik168@163.com>
2011-07-27 21:51:53 +0800

    revise FormTab 4 standard

--- src/esui/FormTab.js
+++ src/esui/FormTab.js
@@ -2,11 +2,14 @@
  * ESUI (Enterprise Simple UI)
  * Copyright 2010 Baidu Inc. All rights reserved.
  * 
- * path:    ui/FormTab.js
+ * path:    esui/FormTab.js
  * desc:    表单Tab控件
  * author:  zhaolei, erik
- * date:    $Date: 2011-04-05 15:57:33 +0800 (二, 05  4 2011) $
  */
+
+
+///import esui.Control;
+///import baidu.lang.inherits;
 
 /**
  * 表单Tab控件
@@ -14,31 +17,42 @@
  * @constructor
  * @param {object} options 构造的选项.
  */
-ui.FormTab = function (options) {
-    this.__initOptions(options);
+esui.FormTab = function ( options ) {
     this.disableHidden = 1;
+
+    esui.Control.call( this, options );
     
-    this.__initOption('autoDisabled', null, 'AUTO_DISABLED');
+    this.__initOption( 'autoDisabled', null, 'AUTO_DISABLED' );
     this.tabs = this.datasource || this.tabs || [];
 };
 
-ui.FormTab.prototype = {
+esui.FormTab.AUTO_DISABLED = 1;
+
+esui.FormTab.prototype = {
     /**
      * 初始化FormTab
      *
      * @public
      */
-    init: function() {
+    init: function () {
         var me = this;
-        if (!me.isInited) {
+        this.activeIndex = this.activeIndex || 0;
+        if ( !me.isInited ) {
             me._initEvent();
             me.isInited = 1;
         }
         
-        setTimeout(function () {
-                me._resetPanel();
-            }, 
-        0);
+        setTimeout( function () {
+                me.setActiveIndex( me.activeIndex );
+            }, 0 );
+    },
+
+    render: function () {
+        this.init();
+    },
+
+    __createMain: function () {
+        return null;
     },
     
     /**
@@ -48,25 +62,25 @@ ui.FormTab.prototype = {
      */
     _initEvent: function () {
         var tabs = this.tabs;
-        var len = tabs.length;
+        var len  = tabs.length;
         var i;
         var tab;
         var radio;
         
-        for (i = 0; i < len; i++) {
-            tab = tabs[i];
+        for ( i = 0; i < len; i++ ) {
+            tab   = tabs[ i ];
             radio = tab.radio;
-            if (radio) {
-                radio = ui.get(radio);
-                radio && (radio.onclick = this._getRadioClickHandler(i));
+            if ( radio ) {
+                radio = esui.util.get( radio );
+                radio && ( radio.onclick = this._getRadioClickHandler( i ) );
             }
         }
     },
     
-    _getRadioClickHandler: function (index) {
+    _getRadioClickHandler: function ( index ) {
         var me = this;
         return function () {
-            return me._select(index);
+            return me._select( index );
         };
     },
     
@@ -76,14 +90,31 @@ ui.FormTab.prototype = {
      * @private
      * @param {number} index 标签序号
      */
-    _select: function (index) {
-        if (this.onchange(index, this.tabs[index]) !== false) {
-            this.activeIndex = index;
-            this._resetPanel();
+    _select: function ( index ) {
+        if ( this.onchange( index, this.tabs[ index ] ) !== false ) {
+            this.setActiveIndex( index );
             return;
         }
 
         return false;
+    },
+    
+    /**
+     * 选择活动标签
+     * 
+     * @public
+     * @param {number} index 标签序号
+     */
+    setActiveIndex: function( index ) {
+        var tabs = this.tabs;
+        var len = tabs.length;
+
+        if ( index >= 0 && index < len ) {
+            esui.util.get( tabs[ index ].radio ).setChecked( true );
+            this.activeIndex = index;
+        }
+
+        this._resetPanel();
     },
     
     onchange: new Function(),
@@ -95,42 +126,31 @@ ui.FormTab.prototype = {
      */
     _resetPanel: function () {
         var tabs = this.tabs;
-        var len = tabs.length;
+        var len  = tabs.length;
         var i;
         var tab;
         var panel;
         var radio;
         var checked;
 
-        for (i = 0; i < len; i++) {
-            tab = tabs[i];
-            radio = tab.radio;
-            panel = tab.panel;
-            panel = panel && baidu.g(panel);
+        for ( i = 0; i < len; i++ ) {
+            tab     = tabs[ i ];
+            radio   = tab.radio;
+            panel   = tab.panel;
+            panel   = panel && baidu.g( panel );
 
-            if (radio && panel) {
-                radio = ui.get(radio);
-                if (radio) {
-                    checked = radio.getChecked();
+            if ( radio && panel ) {
+                radio = esui.util.get( radio );
+                if ( radio ) {
+                    checked = radio.isChecked();
 
                     this.autoDisabled 
-                        && ui.util.disableFormByContainer(panel, !checked);
+                        && esui.util.setDisabledByContainer( panel, !checked );
                     panel.style.display = checked ? '' : 'none';
                 }
             }
         }
-    },
-
-    /**
-     * 释放控件
-     * 
-     * @public
-     */
-    dispose: function () {
-    	var me = this;   
-    	ui.Base.dispose.call(me);
     }
 };
 
-ui.FormTab.AUTO_DISABLED = 1;
-ui.Base.derive(ui.FormTab);
+baidu.inherits( esui.FormTab, esui.Control );
