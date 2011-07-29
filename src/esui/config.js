a3b6008797e048a3a06d76a3623eee0790389e0f
errorrik <erik168@163.com>
2011-07-29 13:37:10 +0800

    add config 4 esui

---
+++ src/esui/config.js
@@ -0,0 +1,14 @@
+/*
+ * ESUI (Enterprise Simple UI)
+ * Copyright 2010 Baidu Inc. All rights reserved.
+ * 
+ * path:    esui/config.js
+ * desc:    ui控件配置项
+ * author:  erik
+ */
+
+
+esui.config = {
+    UI_ATTRIBUTE: 'ui'
+};
+
