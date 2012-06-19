8a9f28db894870362b9d12b74774a878346ba934
erik <erik168@163.com>
2012-06-19 12:01:29 +0800

    fix bug:initer add index

--- src/er/init.js
+++ src/er/init.js
@@ -56,7 +56,11 @@ er.init = function () {
      */
     init.addIniter = function ( initer, opt_index ) {
         if ( typeof opt_index == 'number' ) {
-            initers.splice( opt_index, 0, initer );
+            if ( initers[ opt_index ] ) {
+                initers.splice( opt_index, 0, initer );
+            } else {
+                initers[ opt_index ] = initer;
+            }
         } else {
             initers.push( initer );
         }
